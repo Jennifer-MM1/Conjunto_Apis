4aada16a60b197cabaae860bad74cf16dcfa0d2a
Jennifer-MM1 <jennifermunozmendoza@gmail.com>
2026-06-23 18:58:11 -0600

    chore: add base path to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Conjunto_Apis/', // <--- Añade esto exactamente igual al nombre de tu repo
   plugins: [
     react(),
     tailwindcss(),
